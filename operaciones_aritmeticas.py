cf1fe07e62d432b6b392ad9324d462d4d4d3f714
bb-gil <alexisgil@colegioguanenta.edu.co>
2024-02-22 17:32:33 -0500

    se agrego el diagrama.png, se actualizo el archivo README, y se agrego el codigo phython del programa

---
+++ operaciones_aritmeticas.py
@@ -0,0 +1,29 @@
+# input
+
+X = int(input("Digite el valor x : "))
+Y = int(input("Digite el valor y : "))
+
+#processing
+
+print("------------------------------------")
+
+S = (X +Y)
+R = (X - Y)
+M = (X * Y)
+D = (X / Y)
+DE = (X // Y) 
+mod = (X % Y)
+p = (X ** Y)
+
+# output
+
+print("vamos a ver los resultados de las operaciones")
+print("------------------------------------")
+print( "Suma" ,S)
+print( "Resta" ,R)
+print( "multiplicacion" ,M)
+print( "division" ,D)
+print( "Divicion Entera" ,DE)
+print( "Modulo" ,mod)
+print( "Potencia" ,p)
+print("-------------------------------------")
